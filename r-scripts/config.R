05e064909dfd6ab6bae5e45d6564ace8c912ceb6
Gottfried Hofmann <g@no.spam>
2013-04-16 11:03:02 +0200

    configuration file

---
+++ r-scripts/config.R
@@ -0,0 +1,16 @@
+#this script file stores login credentials and other info is is user-dependent
+
+#ohloh API-Key
+apiKey <- ""
+
+#hostname of your databse
+dbHost <- ""
+
+#name database
+dbName <- ""
+
+#username database
+dbUser <- ""
+
+#password database
+dbPass <- ""
