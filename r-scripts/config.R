ee1fcad92b88946bc9a9800b46ea136b84ecf775
Gottfried Hofmann <g@no.spam>
2014-02-21 13:56:02 +0100

    new configuration options

--- r-scripts/config.R
+++ r-scripts/config.R
@@ -17,19 +17,19 @@ dbPass <- ""
 
 #number of ohloh API calls per day, set a little lower than the actual amount
 #for example if you can make 1000 calls per day, set it to 990 
-apiCalls <- 10
+apiCalls <- 990
 
 #in case of a testrun, existing tables are dropped and new ones created
-testRun <- FALSE
+reBuild <- FALSE
 
 #should the script store each XML-file retrieved locally on disk?
 storeXML <- TRUE
+
+#parse info on programming languages?
+parseLang <- FALSE
 
 #where to store xml-files?
 projectsDir <- "data/xml/projects"
 activity_factsDir <- "data/xml/activity_facts"
 enlistmentsDir <- "data/xml/enlistments"
 languagesDir <- "data/xml/languages"
-
-#parse info on programming languages?
-parseLang <- FALSE
